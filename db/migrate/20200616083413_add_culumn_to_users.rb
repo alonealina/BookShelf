86c9f419c3649706ae99ac25a24d6cce1dd5c885
EC2 Default User <ec2-user@ip-172-31-74-99.ec2.internal>
2020-06-16 08:40:05 +0000

    Add column

---
+++ db/migrate/20200616083413_add_culumn_to_users.rb
@@ -0,0 +1,5 @@
+class AddCulumnToUsers < ActiveRecord::Migration[5.0]
+  def change
+      add_column :users, :sign_in_count, :integer
+  end
+end
---
+++ db/migrate/20200616083607_add_sign_culumn_to_users.rb
@@ -0,0 +1,5 @@
+class AddSignCulumnToUsers < ActiveRecord::Migration[5.0]
+  def change
+    add_column :users, :sign_in_count, :integer
+  end
+end
